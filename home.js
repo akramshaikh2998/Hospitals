f9f399aae9224d800fe2a77fca72cc75464e1c17
akramshaikh2998 <99076951+akramshaikh2998@users.noreply.github.com>
2023-09-17 19:59:15 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,31 @@
+
+// -------------------------------------------- FAQs----------------------------------------
+var faq = document.getElementsByClassName("faq-page");
+var i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
+
+// -------------------------------------------- Carousel-----------------------------------
+$(function() {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+      items: 3,
+      margin: 10,
+      loop: true,
+      nav: true
+    });
+  });
+  
